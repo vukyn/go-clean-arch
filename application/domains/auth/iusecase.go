9fe4646109f3b138021675da93a2aebf7dd60723
Vu Ky <vukynpro@gmail.com>
2023-06-05 22:25:41 +0700

    modify interface

--- application/domains/auth/iusecase.go
+++ application/domains/auth/iusecase.go
@@ -1,12 +1,7 @@
 package auth
 
-import (
-	"boilerplate-clean-arch/models"
-	"context"
-)
-
 type UseCase interface {
-	SignUp(ctx context.Context, username, password, name string) (*models.User, error)
-	SignIn(ctx context.Context, username, password string) (string, error)
-	ParseToken(ctx context.Context, accessToken string) (string, error)
+	// SignUp(ctx context.Context, username, password, name string) (*models.User, error)
+	// SignIn(ctx context.Context, username, password string) (string, error)
+	// ParseToken(ctx context.Context, accessToken string) (string, error)
 }
--- application/domains/auth/usecase/usecase.go
+++ application/domains/auth/usecase/usecase.go
@@ -1,5 +1,12 @@
 package usecase
 
+import "boilerplate-clean-arch/application/domains/auth"
+
 type authUseCase struct {
-	// userRepo auth.UserRepository
+	userRepo auth.Repository
+}
+
+// Constructor
+func NewAuthUseCase(userRepo auth.Repository) auth.UseCase {
+	return &authUseCase{userRepo}
 }
