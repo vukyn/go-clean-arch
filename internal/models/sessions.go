dd4628c5a8d53b0144b432cff8ec463a770d41cb
vuky <kyndv@hasaki.vn>
2023-07-11 17:15:58 +0700

    clear config

--- internal/models/sessions.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-// Session model
-type Session struct {
-	SessionId string    `json:"session_id" redis:"session_id"`
-	UserId    uuid.UUID `json:"user_id" redis:"user_id"`
-}
